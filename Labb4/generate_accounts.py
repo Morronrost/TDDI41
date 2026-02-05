5a56b1c4f1f5552dc5d0533afb7ea8d057506f8c
Fredrik Nilgert <fredrik03@live.se>
2026-02-05 11:51:41 +0100

    fixed generate_accounts.py

--- Labb4/generate_accounts.py
+++ Labb4/generate_accounts.py
@@ -17,29 +17,29 @@ with open(sys.argv[1], "rt", encoding="utf-8") as namefile:
     names = names.split("\n")
     for name in names:
         finalName = ""
-		if (name != ""):
-	        name = name.split()
-	        name = (name[0][:3] + name[-1][:2]).lower()
-	        for letter in name:
-	            good = False
-	            for x in range(len(alphabet)):
-	                if x <= 9:
-	                    if letter == numbers[x]:
-	                        good = True
-	                if letter == alphabet[x]:
-	                    good = True
-	                
-	            if good:
-	                finalName += letter
-	            else:
-	                finalName += alphabet[random.randint(0,(len(alphabet)-1))]
+        name = name.split()
+        if name != "":
+            name = (name[0][:3] + name[-1][:2]).lower()
+            for letter in name:
+                good = False
+                for x in range(len(alphabet)):
+                    if x <= 9:
+                        if letter == numbers[x]:
+                            good = True
+                    if letter == alphabet[x]:
+                        good = True
+                    
+                if good:
+                    finalName += letter
+                else:
+                    finalName += alphabet[random.randint(0,(len(alphabet)-1))]
+        else:
+            print("Account creation complete")
 
-	        finalName = (finalName + str(random.randint(0,9)) + str(random.randint(0,9)) + str(random.randint(0,9)))
-	        password = getPass()
-	        subprocess.run(['useradd', '-ms', '/bin/bash', finalName ])
-	        subprocess.run(['passwd', finalName], input=f"{password}\n{password}\n", text=True)
-		else:
-			print("All users created")
+        finalName = (finalName + str(random.randint(0,9)) + str(random.randint(0,9)) + str(random.randint(0,9)))
+        password = getPass()
+        subprocess.run(['useradd', '-ms', '/bin/bash', finalName ])
+        subprocess.run(['passwd', finalName], input=f"{password}\n{password}\n", text=True)
         
         #print("ID: " + finalName)
         #print("Password: " + password)
